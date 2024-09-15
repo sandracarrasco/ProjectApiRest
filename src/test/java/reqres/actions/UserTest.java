3b8491a24c29d88f220c03cd5e6eb7db8d1b1042
SandraCalliconde <sady240299@gmail.com>
2024-09-14 23:43:12 -0400

    Se agregó comentarios de los tests

--- src/test/java/reqres/actions/UserTest.java
+++ src/test/java/reqres/actions/UserTest.java
@@ -6,7 +6,9 @@ import reqres.general.ReqresApiBase;
 
 public class UserTest extends ReqresApiBase {
 
-
+     /**
+     * 1. Consultar los usuarios de la página 2
+     */
     @Test
     public void usersPage() {
         Response response = UserRequest.getUsers(requestSpecification,"2");
@@ -14,6 +16,9 @@ public class UserTest extends ReqresApiBase {
         UserQuestions.validateJsonSchema(response, "reqres/UsersJsonSchema.json");
     }
 
+     /**
+     * 2. Consultar los datos del usuario 12
+     */
     @Test
     public void userEspecifique() {
         Response response = UserRequest.getUser(requestSpecification,"12");
@@ -21,6 +26,9 @@ public class UserTest extends ReqresApiBase {
         UserQuestions.validateJsonSchema(response, "reqres/UserJsonSchema.json");
     }
 
+     /**
+     * 3. Consultar los datos de un usuario inexistente
+     */
     @Test
     public void userInexistente() {
         Response response = UserRequest.getUser(requestSpecification,"23");
@@ -28,7 +36,9 @@ public class UserTest extends ReqresApiBase {
         UserQuestions.validateJsonSchema(response, "reqres/UserNotFound.json");
     }
 
-
+     /**
+     * 4. Crear un usuario
+     */
     @Test
     public void createUser() {
         String payload = UserModel.userPayload("morpheus", "leader");
@@ -37,7 +47,9 @@ public class UserTest extends ReqresApiBase {
         UserQuestions.validateJsonSchema(response, "reqres/UserCreatedJsonSchema.json");
     }
 
-
+     /**
+     * 5. Consumir el servicio "Delayed Response"
+     */
     @Test
     public void consumeService() {
         Response response = UserRequest.getUserDelayed(requestSpecification,"3");
